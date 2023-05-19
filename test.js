fad776bc02a903621f696d4eeac50dd052763e9c
Omar <oromero@unsa.edu.pe>
2023-05-19 18:40:24 -0500

    JavaScript Formularios y Eventos

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-document.getElementByDi("resultadoSuma").innerHTML= 1+2;
+//document.getElementByDi("resultadoSuma").innerHTML= 1+2;
+document.addEventListener("DOMContentLoaded", function() {
+    const name = document.querySelector("#userName");
+    const button = document.querySelector("#enviar");
+    button.disable = true;
+
+    name.onkeyup = () =>{
+        if(name.ariaValueMax.length>0){
+            button.disable=false;
+        }else{
+            button.disable=true;
+        }
+    };
+    document.querySelector("#form1").onsubmit = () =>{
+        document.getElementById("resultado").innerHTML="Hola" + name.value;
+        //no recargar la página
+        return false;
+    };
+});
