6c1d94f0dedd11c10541e469ec39bfe28abf6cae
Omar <oromero@unsa.edu.pe>
2023-05-21 16:39:30 -0500

    Se completo las diapositivas AJAX y NodeJS, hasta la carpeta pub

--- markDownServer.js
+++ markDownServer.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.use(express.static("pub"));
+
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
 });
